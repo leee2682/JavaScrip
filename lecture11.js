38e6c72430cf9510c58cce99cfb7290314ace095
leee2682 <78770258+leee2682@users.noreply.github.com>
2021-03-08 17:35:50 +0900

    Add files via upload

---
+++ lecture11.js
@@ -0,0 +1,15 @@
+// var a = 1;
+// console.log(a);
+// console.log(--a);
+// console.log(a);
+
+// var a = 1;
+// console.log(a);
+// console.log(++a);
+// console.log(a);
+
+var a = 10;
+var b = 3;
+var c = a + b;
+var d = a - b;
+var e = a * b;
